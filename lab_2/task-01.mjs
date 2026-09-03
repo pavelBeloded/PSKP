476b0d74046b0f676c3e04c982239582e78d2c29
PavelBeloded <beloded_2007@bk.ru>
2026-09-03 09:04:24 +0300

    feat: lab 2 task 1

---
+++ lab_2/task-01.mjs
@@ -0,0 +1,34 @@
+import http from "http";
+import * as fs from "node:fs";
+import * as path from "node:path";
+import { fileURLToPath } from "node:url"
+
+
+const server = http.createServer((req, res) => {
+
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const filePath = path.join(__dirname, 'index.html');
+  const {method, url} = req;
+
+  if(url === '/html' && method === "GET") {
+    fs.readFile(filePath, (err, data) => {
+
+      if(err) {
+        res.writeHead(404, {"Content-Type": "text/html"});
+        res.end('File Not Found');
+        return;
+      }
+      res.writeHead(200, {"Content-Type": "text/html"});
+      res.end(data);
+
+    })
+
+  }
+
+})
+
+
+server.listen(5000, ()=> {
+  console.log("Server started on port 5000");
+})
