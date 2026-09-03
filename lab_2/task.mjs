725d54afa36665197cc5c18af52213e66b099f54
PavelBeloded <beloded_2007@bk.ru>
2026-09-03 10:01:22 +0300

    feat: lab 2 is done

---
+++ lab_2/task.mjs
@@ -0,0 +1,94 @@
+import http from "http";
+import * as fs from "node:fs";
+import * as path from "node:path";
+import { fileURLToPath } from "node:url"
+
+
+const server = http.createServer((req, res) => {
+
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const {method, url} = req;
+
+  if(url === '/html' && method === "GET") {
+    const filePath = path.join(__dirname, 'index.html');
+    fs.readFile(filePath, (err, data) => {
+
+      if(err) {
+        res.writeHead(404, {"Content-Type": "text/html"});
+        res.end('File Not Found');
+        return;
+      }
+      res.writeHead(200, {"Content-Type": "text/html"});
+      res.end(data);
+
+    })
+  }
+
+  if(url === '/png' && method === "GET") {
+    const imgPath = path.join(__dirname, 'image.png');
+
+    fs.readFile(imgPath, (err, data) => {
+      if(err) {
+        res.writeHead(404, {"Content-Type": "text/html"});
+        res.end('File Not Found');
+        return;
+      }
+
+      res.writeHead(200, {"Content-Type": "image/png"});
+      res.end(data);
+    })
+  }
+
+  if(url === '/api/name' && method === "GET") {
+    res.writeHead(200, { "Content-Type": "text/plain; charset=utf-8" });
+    res.end('Белодед Павел Вячеславович');
+  }
+
+  if (url === '/xmlhttprequest' && method === 'GET') {
+    const filePath = path.join(__dirname, 'xmlhttprequest.html');
+
+    fs.readFile(filePath, (err, data) => {
+      if (err) {
+        res.writeHead(404, {"Content-Type": "text/plain; charset=utf-8"});
+        res.end('File Not Found');
+        return;
+      }
+      res.writeHead(200, {"Content-Type": "text/html; charset=utf-8"});
+      res.end(data);
+    });
+  }
+
+  if(url === '/fetch' && method === "GET") {
+    const filePath = path.join(__dirname, 'fetch.html');
+
+    fs.readFile(filePath, (err, data) => {
+      if (err) {
+        res.writeHead(404, {"Content-Type": "text/plain; charset=utf-8"});
+        res.end('File Not Found');
+        return;
+      }
+      res.writeHead(200, {"Content-Type": "text/html; charset=utf-8"});
+      res.end(data);
+    });
+  }
+
+  if(url === '/jQuery' && method === "GET") {
+    const filePath = path.join(__dirname, 'ajax.html');
+
+    fs.readFile(filePath, (err, data) => {
+      if (err) {
+        res.writeHead(404, {"Content-Type": "text/plain; charset=utf-8"});
+        res.end('File Not Found');
+        return;
+      }
+      res.writeHead(200, {"Content-Type": "text/html; charset=utf-8"});
+      res.end(data);
+    });
+  }
+})
+
+
+server.listen(5000, ()=> {
+  console.log("Server started on port 5000");
+})
