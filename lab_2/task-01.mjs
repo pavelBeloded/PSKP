bda6e59c64395ff31a1704e7e491ccb350db3261
PavelBeloded <beloded_2007@bk.ru>
2026-09-03 09:09:38 +0300

    feat: lab 2 task 2

--- lab_2/task-01.mjs
+++ lab_2/task-01.mjs
@@ -8,10 +8,10 @@ const server = http.createServer((req, res) => {
 
   const __filename = fileURLToPath(import.meta.url);
   const __dirname = path.dirname(__filename);
-  const filePath = path.join(__dirname, 'index.html');
   const {method, url} = req;
 
   if(url === '/html' && method === "GET") {
+    const filePath = path.join(__dirname, 'index.html');
     fs.readFile(filePath, (err, data) => {
 
       if(err) {
@@ -23,7 +23,21 @@ const server = http.createServer((req, res) => {
       res.end(data);
 
     })
+  }
 
+  if(url === '/png' && method === "GET") {
+    const imgPath = path.join(__dirname, 'image.png');
+
+    fs.readFile(imgPath, (err, data) => {
+      if(err) {
+        res.writeHead(404, {"Content-Type": "text/html"});
+        res.end('File Not Found');
+        return;
+      }
+
+      res.writeHead(200, {"Content-Type": "image/png"});
+      res.end(data);
+    })
   }
 
 })
